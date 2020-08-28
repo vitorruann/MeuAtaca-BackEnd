f8522a695130453d7ad2c0f41ff8e9d02f466ce0
unknown <vitorruann@gmail.com>
2020-08-27 21:39:15 -0300

    Adicionado mongoose, alterado estrutura, criado pasta app e config

--- meuAtacaBack/src/routes.js
+++ meuAtacaBack/src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const TesteController = require('./controllers/TesteController');
+const TesteController = require('./app/controllers/TesteController');
 
 router.get('/', TesteController.store);
 
